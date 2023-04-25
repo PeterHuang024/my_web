f1f24f26b44914ced9288e19a7b0cc9931cbbeff
peterhuang <kobe24abc@gmail.com>
2023-04-25 12:43:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
+with st.sidebar:
+	st.write("I'm Tzu-Yang, can call me Peter. Currently am Analystics & Insights Professional at Amazon Ads. But laid off.")
+
 st.write('Hello world!')
