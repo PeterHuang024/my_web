ab686106c45ed29a5f68f010dcfbccec8f4a471b
peterhuang <kobe24abc@gmail.com>
2023-04-25 17:57:11 -0400

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-with st.sidebar:
-	st.write("I'm Tzu-Yang, can call me Peter. Currently am Analystics & Insights Professional at Amazon Ads. But laid off.")
-
-st.write('Hello world!')
