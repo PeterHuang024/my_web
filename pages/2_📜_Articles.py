55481f538c8ee97df0a509c8a87ef05b3b424af1
peterhuang <kobe24abc@gmail.com>
2024-06-02 11:13:12 -0500

    Create 2_📜_Articles.py

---
+++ pages/2_📜_Articles.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import json
+
+st.header("Articles",divider='rainbow')
+
+with st.sidebar:
+	st.image("images/linkedin_icon.png", use_column_width=True)
+	st.title("""Tzu-Yang (Peter) Huang""")
+	st.markdown("- Data Scientist")
