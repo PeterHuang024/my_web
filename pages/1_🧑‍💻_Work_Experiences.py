77794e2ffb1bf240e9262e530fd829763cfa7f5f
peterhuang <kobe24abc@gmail.com>
2024-06-02 23:52:54 -0500

    Update 1_🧑‍💻_Work_Experiences.py

--- pages/1_🧑‍💻_Work_Experiences.py
+++ pages/1_🧑‍💻_Work_Experiences.py
@@ -3,8 +3,25 @@ import json
 
 st.header("Work Experience",divider='rainbow')
 
+st.markdown("""
+    <style>
+    	[data-testid='stAppViewContainer'] {
+    		background-color: #f9f9f9;
+			opacity: 1;
+			background-image: radial-gradient(#f7ca45 2px, #f9f9f9 2px);
+			background-size: 40px 40px;
+			background-position: top left;
+			background-attachment: fixed;
+    	}
+        [data-testid=stSidebar] {
+            background-color: #FFF8DC;
+        }
+      
+    </style>
+    """, unsafe_allow_html=True)
+
 with st.sidebar:
-	st.image("images/linkedin_icon.png", use_column_width=True)
+	st.image("images/photo.png", use_column_width=True)
 	st.title("""Tzu-Yang (Peter) Huang""")
 	st.markdown("- Data Scientist")
 
@@ -14,11 +31,10 @@ def add_space(word1, word2):
 	return tot - len(word1) - len(word2)
 
 def display_work_experience(entry):
-	space1 = "&nbsp;" * entry['space1']
-	space2 = "&nbsp;" * entry['space2']
+	space = "&nbsp;" * 3
 
-	with st.expander(f"{entry['title']} {space1} {entry['duration']}", expanded=True):
-		st.write(f"[{entry['company']}]({entry['company_link']}) {space2} {entry['location']}")
+	with st.expander(f"{entry['title']}, {space} {entry['duration']}", expanded=True):
+		st.write(f"[{entry['company']}]({entry['company_link']}), {space} {entry['location']}")
 		st.write(entry["description"])
 
 		if len(entry['links']) > 0:
